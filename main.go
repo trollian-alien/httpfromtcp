db475b4640e75ab5993f3e5fceaf05df89541f37
trollian-alien <epicscientician@gmail.com>
2025-12-25 13:48:01 +0300

    reading 8 bits at time and parsing them

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,15 +13,23 @@ func main() {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
+	b := make([]byte, 0)
+	fmt.Print("read: ")
 	defer file.Close()
 	for {
-		b := make([]byte, 8)
-		n, err := file.Read(b)
+		c := make([]byte, 8)
+		n, err := file.Read(c)
+		b = append(b, c[:n]...)
 		if err == io.EOF {
 			break
 		} else if err != nil {
 			os.Exit(1)
 		}
-		fmt.Printf("read: %s\n", b[:n])
 	}
+	s := string(b)
+	s = strings.Replace(s, "\n", "\nread: ", -1)
+	s = strings.TrimSuffix(s, "\nread: ")
+    s = strings.TrimSuffix(s, "\n")
+
+	fmt.Println(s)
 }
