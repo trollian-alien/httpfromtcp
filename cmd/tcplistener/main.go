f743f85e7d091bf712eb914fcc88bc5d692a4ed9
trollian-alien <epicscientician@gmail.com>
2025-12-27 11:52:12 +0300

    bugfix

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -3,40 +3,59 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
-	"strings"
 	"log"
 	"net"
+	"strings"
 )
 
 const port = ":42069"
 
 func getLinesChannel(f io.ReadCloser) <-chan string {
 	ch := make(chan string)
-	s := ""
-	temp := ""
 	go func() {
 		defer f.Close()
+		defer close(ch)
+		s := "" // for storing partial lines that must be carried to next iteration of loop
 		for {
-			s += temp
-			temp = ""
 			b := make([]byte, 8)
 			n, err := f.Read(b)
 			str := string(b[:n])
-			parts := strings.Split(str, "\n")
+			parts := strings.Split(str, "\r\n")
 
-			s += parts[0]
-			if len(parts) == 2 {
-				temp += parts[1]
-				ch <- s
-				s = ""
-			} 
-			
+			//adding to the initial partial line
+			if len(parts) > 0 {
+				s += parts[0]
+			}
+
+			// middle parts are full lines
+			l := len(parts)
+			for i := 1; i < l-1; i++ {
+				if s != "" {
+					ch <- s
+				}
+				s = parts[i]
+			}
+
+			// handle the last part
+			if len(parts) > 1 {
+				last := parts[l-1]
+				if strings.HasSuffix(str, "\r\n") {
+					// line finished in this chunk
+					ch <- s
+					s = ""
+				} else {
+					// still partial, carry over
+					s += last
+				}
+			}
+
 			if err == io.EOF {
-				close(ch)
+				if s != "" {
+					ch <- s
+				}
 				break
 			} else if err != nil {
-				os.Exit(1)
+				log.Fatalf("error listening TCP traffic: %s\n", err.Error())
 			}
 		}
 	}()
@@ -49,7 +68,7 @@ func main() {
 		log.Fatalf("error listening for TCP traffic: %s\n", err.Error())
 	}
 	defer listener.Close()
-	
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -58,8 +77,8 @@ func main() {
 		fmt.Println("Accepted connection from", conn.RemoteAddr())
 
 		ch := getLinesChannel(conn)
-		for line := range(ch) {
-		fmt.Println(line)
+		for line := range ch {
+			fmt.Println(line)
 		}
 		fmt.Println("Connection closed")
 	}
