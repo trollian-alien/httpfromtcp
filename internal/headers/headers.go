f427a00dc960c083b27d6b5084f53e2892a4a777
trollian-alien <epicscientician@gmail.com>
2025-12-29 14:25:12 +0300

    headers parsing logic

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -11,6 +11,16 @@ func NewHeaders() Headers {
 	return make(Headers)
 }
 
+func isTokenChar(r rune) bool {
+    if r >= 'a' && r <= 'z' { return true }
+    if r >= '0' && r <= '9' { return true }
+    switch r {
+    case '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+        return true
+    }
+    return false
+}
+
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	str := string(data)
 	index := strings.Index(str, "\r\n")
@@ -22,12 +32,28 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	}
 	
 	keyAndValue := str[:index]
+	//extracting the key and value and checking syntax
 	key, value, found := strings.Cut(keyAndValue, ":")
 	if found == false {
 		return 0, false, fmt.Errorf("invalid header syntax: colon missing")
 	} else if strings.HasSuffix(key, " ") {
 		return 0, false, fmt.Errorf("invalid header field-name %v", key)
 	}
-	h[strings.TrimSpace(key)] = strings.TrimSpace(value)
+	key = strings.ToLower(strings.TrimSpace(key))
+	for _, r := range key {
+		if !isTokenChar(r) {
+			return 0, false, fmt.Errorf("invalid header key: %v", key)
+		}
+	}
+	value = strings.TrimSpace(value) 
+
+	//adding the value to the key map
+	oldVal, ok := h[key]
+	if !ok {
+		h[key] = strings.TrimSpace(value)
+	} else {
+		h[key] = oldVal + ", " + value
+	}
+
 	return index + 2, false, nil
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -13,7 +13,7 @@ func TestHeadersParse(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 
@@ -23,7 +23,7 @@ func TestHeadersParse(t *testing.T) {
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 57, n)
 	assert.False(t, done)
 
@@ -34,7 +34,7 @@ func TestHeadersParse(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, headers)
 	assert.Equal(t, "localhost:42069", headers["host"])
-	assert.Equal(t, "curl/7.81.0", headers["User-Agent"])
+	assert.Equal(t, "curl/7.81.0", headers["user-agent"])
 	assert.Equal(t, 25, n)
 	assert.False(t, done)
 
@@ -55,4 +55,23 @@ func TestHeadersParse(t *testing.T) {
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+
+	// Test: Invalid character header
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Same header key
+	headers = map[string]string{"host": "localhost:8000"}
+	data = []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:8000, localhost:42069", headers["host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
 }
+
